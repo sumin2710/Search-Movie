7e84c8d72470067a596363d09c976eff86a7f62c
sumin2710 <su91582710@gmail.com>
2024-01-09 11:38:25 +0900

    feedback 반영

--- movie_search.js
+++ movie_search.js
@@ -1,34 +1,21 @@
 import config from './apiKey.js';
-const apiKey = config.API_KEY;
-const authorization = config.AUTHORIZATION;
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: authorization
-  }
+// ============================================================ //
+// 전역 변수
+// ============================================================ //
+const API_KEY = config.API_KEY;
+const AUTH = config.AUTHORIZATION;
+const OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: AUTH
+    }
 };
 
-// 로드가 완료되면, 영화들을 불러오기
-document.addEventListener('DOMContentLoaded', async function () {
-    const $movieContainer = document.querySelector('#movie-container');
-    try {
-        const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`, options);
-        const data = await res.json();
-
-        if (data.results) {
-            data.results.forEach((movie) => {
-                createCard(movie, $movieContainer);
-            });
-        }else{
-            $movieContainer.textContent = 'NO MOVIES FOUND';
-        }
-    } catch (err){
-        console.error("Error fetching data: ", err);
-        $movieContainer.textContent = 'ERROR FETCHING DATA';
-    }
-});
+// ============================================================ //
+// 렌더링
+// ============================================================ //
 
 // 영화 하나 당 카드 하나 생성하기
 function createCard(movie, container) {
@@ -63,69 +50,65 @@ function createCard(movie, container) {
     container.appendChild(card);
 }
 
-// 검색 버튼을 클릭하면 searchMovie나 searchMovie2 수행
-document.getElementById('search-btn').addEventListener('click', searchMovie2);
+// ============================================================ //
+// 이벤트
+// ============================================================ //
 
-// 엔터를 누르면 검색 버튼을 누른것과 동일한 효과를 주자
-document.getElementById('search-input').addEventListener('keyup', function(event){
-    if(event.key === 'Enter') document.getElementById('search-btn').click();
-});
-
-async function searchMovie(){
+async function searchMovie() {
     const $movieContainer = document.querySelector('#movie-container');
     $movieContainer.innerHTML = ''; // 전의 카드들 지우기
     const $searchInput = document.querySelector('#search-input').value.toLowerCase(); // 대소문자 관계없이 검색 가능하게 하기
 
     // 빈 문자열이면, 
-    if($searchInput === ''){
+    if ($searchInput === '') {
         $movieContainer.textContent = 'You haven\'t entered movie title yet.';
         return;
     }
 
-    try{
-        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent($searchInput)}`);
+    try {
+        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent($searchInput)}`);
         const data = await res.json();
-    
-        if(data.results.length !== 0){
+
+        if (data.results.length !== 0) {
             data.results.forEach((movie) => {
                 createCard(movie, $movieContainer);
             });
-        }else{
+        } else {
             $movieContainer.textContent = 'NO RESULTS';
         }
-    }catch(err){
+    } catch (err) {
         console.error("Error fetching data: ", err);
         $movieContainer.textContent = 'ERROR FETCHING DATA';
     }
 }
 
 // filter로 검색기능 구현해봄 
-async function searchMovie2(){
+async function searchMovie2() {
     const $movieContainer = document.querySelector('#movie-container');
     $movieContainer.innerHTML = ''; // 전의 카드들 지우기
     const $searchInput = document.querySelector('#search-input').value.toLowerCase(); // 대소문자 관계없이 검색 가능하게 하기
 
     // 빈 문자열이면, 
-    if($searchInput === ''){
+    if ($searchInput === '') {
         $movieContainer.textContent = 'You haven\'t entered movie title yet.';
         return;
     }
 
-    try{
-        const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`, options);
+    try {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`, OPTIONS);
         const data = await res.json();
-      
-        if(data.results.length !== 0){
+
+        if (data.results.length !== 0) {
             let filteredMovies = data.results.filter(v => v.title.toLowerCase().includes($searchInput));
-            if(filteredMovies.length === 0) $movieContainer.textContent = 'NO RESULTS';
-        
+            if (filteredMovies.length === 0) $movieContainer.textContent = 'NO RESULTS';
+
             filteredMovies.forEach((movie) => {
                 createCard(movie, $movieContainer);
             });
-        }else{
+        } else {
             $movieContainer.textContent = 'NO RESULTS';
         }
-    }catch(err){
+    } catch (err) {
         console.error("Error fetching data: ", err);
         $movieContainer.textContent = 'ERROR FETCHING DATA';
     }
@@ -135,3 +118,40 @@ async function searchMovie2(){
 const showId = (id) => {
     alert(`영화 id: ${id}`);
 }
+
+// init 함수 -> 기본 영화들을 불러오기 
+const initMovie = async () => {
+    const $movieContainer = document.querySelector('#movie-container');
+    try {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`, OPTIONS);
+        const data = await res.json();
+
+        if (data.results) {
+            data.results.forEach((movie) => {
+                createCard(movie, $movieContainer);
+            });
+        } else {
+            $movieContainer.textContent = 'NO MOVIES FOUND';
+        }
+    } catch (err) {
+        console.error("Error fetching data: ", err);
+        $movieContainer.textContent = 'ERROR FETCHING DATA';
+    }
+};
+
+// ============================================================ //
+// 함수 진입점
+// ============================================================ //
+
+// 로드가 완료되면 전체 함수 진입
+document.addEventListener('DOMContentLoaded', async function () {
+    // 검색 버튼을 클릭하면 searchMovie나 searchMovie2 수행
+    document.getElementById('search-btn').addEventListener('click', searchMovie2);
+
+    // 엔터를 누르면 검색 버튼을 누른것과 동일한 효과를 주자
+    document.getElementById('search-input').addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') searchMovie2();
+    });
+
+    await initMovie();
+});
